ec4991be31957b8f1c902767466b682963f5bdb0
Pheszm <carlwynegallardo@gmail.com>
2023-12-05 15:49:30 +0800

    Major Update

--- Extentions/script.js
+++ Extentions/script.js
@@ -27,14 +27,15 @@ function updateHighlighting() {
 
 ///HEADER NAVIGATION VISIBILITY
 const NavigationBar = document.getElementById('Header');
-            
+
 function HeaderGoesVisible() {
-    if (window.scrollY >= 40 * (window.innerWidth / 100)) {
-        NavigationBar.style.visibility = 'visible';
-    } else {
-        NavigationBar.style.visibility = 'hidden';
-    }
+  if (window.scrollY >= 30 * (window.innerWidth / 100)) {
+    NavigationBar.classList.add('visible');
+  } else {
+    NavigationBar.classList.remove('visible');
+  }
 }
+
 window.addEventListener('load', HeaderGoesVisible);
 window.addEventListener('scroll', HeaderGoesVisible);
 
@@ -57,3 +58,90 @@ window.addEventListener('scroll', updateBackToTopButtonVisibility);
 backToTopButton.addEventListener('click', () => {
     window.scrollTo({ top: 0, behavior: 'smooth' });
 });
+
+
+
+
+///ABOUT VISIBLE
+const AboutSecc = document.getElementById('About');
+
+function AboutGoesVisible() {
+  const scrollThreshold = 30 * (window.innerWidth / 100);
+  
+  if (window.scrollY >= scrollThreshold) {
+    AboutSecc.style.opacity = '1';
+  } else {
+    AboutSecc.style.opacity = '0';
+  }
+}
+
+window.addEventListener('load', AboutGoesVisible);
+window.addEventListener('scroll', AboutGoesVisible);
+
+
+
+///RESUME VISIBLE
+const ResumeSection = document.getElementById('ResumeSection');
+const scrollThreshold = 65 * (window.innerWidth / 100); // Adjust the scroll threshold as needed
+
+function ResumeSectionVisibility() {
+  if (window.scrollY >= scrollThreshold) {
+    ResumeSection.style.opacity = '1';
+  } else {
+    ResumeSection.style.opacity = '0';
+  }
+}
+
+window.addEventListener('load', ResumeSectionVisibility);
+window.addEventListener('scroll', ResumeSectionVisibility);
+
+
+///AWARDS VISIBLE
+const AwardsSection = document.getElementById('AwardsSection');
+const AwardsscrollThreshold = 105 * (window.innerWidth / 100); // Adjust the scroll threshold as needed
+
+function AwardsSectionVisibility() {
+  if (window.scrollY >= AwardsscrollThreshold) {
+    AwardsSection.style.opacity = '1';
+  } else {
+    AwardsSection.style.opacity = '0';
+  }
+}
+
+window.addEventListener('load', AwardsSectionVisibility);
+window.addEventListener('scroll', AwardsSectionVisibility);
+
+
+///CONTACT VISIBLE
+const contactSection = document.getElementById('ContactSection');
+const ContactscrollThreshold = 145 * (window.innerWidth / 100); // Adjust the scroll threshold as needed
+
+function handleContactSectionVisibility() {
+  if (window.scrollY >= ContactscrollThreshold) {
+    contactSection.classList.add('show');
+  } else {
+    contactSection.classList.remove('show');
+  }
+}
+
+window.addEventListener('load', handleContactSectionVisibility);
+window.addEventListener('scroll', handleContactSectionVisibility);
+
+
+
+
+
+///ALTERNATE TITLE SHOWING
+const title1 = document.querySelector('.Title1');
+const title2 = document.querySelector('.Title2');
+
+function toggleTitles() {
+  setInterval(() => {
+    title1.style.opacity = (title1.style.opacity === '0' || title1.style.opacity === '') ? '1' : '0';
+    title2.style.opacity = (title2.style.opacity === '0' || title2.style.opacity === '') ? '1' : '0';
+  }, 1000); // 5 seconds interval (5000 milliseconds)
+}
+
+toggleTitles();
+
+
